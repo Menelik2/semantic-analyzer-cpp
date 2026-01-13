2d922c006515f7f28f5346d72ef255dfaa791121
Menelik Admasu <minilover2014@gmail.com>
2026-01-13 14:13:08 -0300

    Update symbol.h

--- include/symbol.h
+++ include/symbol.h
@@ -1,1 +1,12 @@
+#pragma once
+#include <string>
+
+enum class Mutability { MUTABLE, IMMUTABLE };
+
+struct Symbol {
+    std::string name;
+    std::string type;
+    Mutability mutability;
+    bool initialized;
+};
 
