dab6f06e37b660ffbd5f203e115e85536382dfa2
Menelik Admasu <minilover2014@gmail.com>
2026-01-13 14:14:12 -0300

    Define ASTVisitor interface with visit methods

--- include/visitor.h
+++ include/visitor.h
@@ -1,1 +1,11 @@
 
+#pragma once
+#include "ast.h"
+
+struct ASTVisitor {
+    virtual void visit(IntLiteral&) = 0;
+    virtual void visit(VarExpr&) = 0;
+    virtual void visit(VarDecl&) = 0;
+    virtual void visit(AssignStmt&) = 0;
+    virtual void visit(Block&) = 0;
+};
