b4350ed8f22c169b903623825fc71cf5f6015c51
Menelik Admasu <minilover2014@gmail.com>
2026-01-13 14:09:01 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,23 @@
+#include "semantic_analyzer.h"
+#include <memory>
+
+int main() {
+    Block program;
+
+    program.statements.push_back(
+        std::make_unique<VarDecl>(
+            "x", "int", false,
+            std::make_unique<IntLiteral>(10)
+        )
+    );
+
+    program.statements.push_back(
+        std::make_unique<AssignStmt>(
+            "x", std::make_unique<IntLiteral>(20)
+        )
+    );
+
+    SemanticAnalyzer analyzer;
+    program.accept(analyzer);
+}
 
