da217bf1ecce3f634340bc6e8b8ea4b9436e1f65
Menelik Admasu <minilover2014@gmail.com>
2026-01-13 14:12:15 -0300

    Implement SymbolTable class with scope management
    Added SymbolTable class for managing symbol scopes.

--- include/symbol_table.h
+++ include/symbol_table.h
@@ -1,1 +1,33 @@
 
+#pragma once
+#include "symbol.h"
+#include <unordered_map>
+#include <stack>
+#include <stdexcept>
+
+class SymbolTable {
+    std::stack<std::unordered_map<std::string, Symbol>> scopes;
+
+public:
+    SymbolTable() { enterScope(); }
+
+    void enterScope() { scopes.push({}); }
+    void exitScope()  { scopes.pop(); }
+
+    void declare(const Symbol& sym) {
+        auto& scope = scopes.top();
+        if (scope.count(sym.name))
+            throw std::runtime_error("Redeclaration of '" + sym.name + "'");
+        scope[sym.name] = sym;
+    }
+
+    Symbol* lookup(const std::string& name) {
+        auto temp = scopes;
+        while (!temp.empty()) {
+            auto& s = temp.top();
+            if (s.count(name)) return &s[name];
+            temp.pop();
+        }
+        return nullptr;
+    }
+};
