bf1dc83b602d3cfb4186d83210dd5451c98a121f
Menelik Admasu <minilover2014@gmail.com>
2026-01-13 14:16:33 -0300

    Implement accept methods for AST nodes

--- src/ast.cpp
+++ src/ast.cpp
@@ -1,1 +1,10 @@
 
+// ast.cpp
+#include "ast.h"
+#include "visitor.h"
+
+void IntLiteral::accept(ASTVisitor& v) { v.visit(*this); }
+void VarExpr::accept(ASTVisitor& v)    { v.visit(*this); }
+void VarDecl::accept(ASTVisitor& v)    { v.visit(*this); }
+void AssignStmt::accept(ASTVisitor& v) { v.visit(*this); }
+void Block::accept(ASTVisitor& v)       { v.visit(*this); }
